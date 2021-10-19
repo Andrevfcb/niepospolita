a47795629ffac154b89554b50981ce40cc475cfd
Adrian Majewski <andrevfcb@gmail.com>
2021-10-19 19:10:21 +0200

    modify adminheader.js

--- src/Components/Admin/AdminHeader.js
+++ src/Components/Admin/AdminHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import "./AdminHeader.css"
+import "./AdminHeader.css";
+import { Link } from 'react-router-dom';
 
 const AdminHeader = () => {
     return (
         <div className="admin-header">
-            Wróć na stronę główną
+            <Link to="/">Wróć na stronę główną</Link>
         </div>
     )
 }
